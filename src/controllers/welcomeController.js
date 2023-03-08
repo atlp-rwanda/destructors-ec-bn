680cd28a4702a4d4f2c2312905dc19c71afdf377
katros1 <katros250@gmail.com>
2023-03-08 17:58:33 +0200

    chore(postgresDB and sequelize):DB configuration

--- src/controllers/welcomeController.js
+++ src/controllers/welcomeController.js
@@ -2,4 +2,7 @@ const welcomeController = (req, res) => {
   res.send('Welcome destructors! This a welcomeController');
 };
 
+
+
 export default welcomeController;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import app from './app.js';
+import app, {connectDB} from './app';
+
 import 'dotenv/config';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+await connectDB();
   console.log(`Server is listening on port ${port}`);
 });
