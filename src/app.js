bb26d47f683950ecd1ffcf1990fecbc3f3867a00
charlotte divine <divinemaina@gmail.com>
2023-03-08 17:34:00 +0200

    Revert "Revert "Revert "This is a combination of all commits."""
    This reverts commit 11373fb6bba681ae6f6d72e2d9294c02719eef15.

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello, There! this is Destructors ecommerce team project.');
-});
-
-export default app;
