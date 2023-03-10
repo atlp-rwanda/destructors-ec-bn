7ac620c6287d5d95ed6a75f7bc7c8288d6503d82
Andela Rwanda <andelarwanda@Andelas-MBP.mshome.net>
2023-03-10 11:18:32 +0200

    ft(google-login-authentication):add  user authentication by google

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL,
+    url: 'postgres://postgres:bukarani@127.0.0.1:5432/prjtdev_db ',
     dialect: "postgres",
   },
   test: {
