85e410c5c068696ebb21e17f9502ebc7cbdd9639
katros1 <katros250@gmail.com>
2023-03-27 14:53:53 +0200

    feat(retrieve items):user should retrieve an item

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,10 +1,13 @@
 /* eslint-disable object-curly-newline */
-import { createProduct } from '../services/product.service.js';
+import { createProduct, findProduct, findProducts } from '../services/product.service.js';
+import verfyToken from '../utils/verifytoken.js';
+import 'dotenv/config';
+
 const createProducts = async (req, res) => {
   try {
     const { name, price, quantity, bonus, expiryDate, categoryId } = req.body;
     const images = req.files.map((img) => img.path);
-    const userId = req.user.id;
+    const sellerId = req.user.id;
     const productDetails = {
       name,
       price,
@@ -12,7 +15,7 @@ const createProducts = async (req, res) => {
       bonus,
       expiryDate,
       categoryId,
-      userId,
+      sellerId,
       images,
     };
 
@@ -28,4 +31,51 @@ const createProducts = async (req, res) => {
   }
 };
 
-export { createProducts };
+const retrieveItem = async (req, res) => {
+  const itemId = req.params.id;
+  const token = req.header('Authorization').split(' ')[1];
+ const user = verfyToken(token, process.env.JWT_SECRET);
+console.log(user.data.role);
+ if (user.data.role == 'seller'){
+  const item = await findProduct(itemId, 'seller', user.data.id);
+  console.log(item);
+  console.log(user.data.id);
+  if(!item){
+    return res.status(400).json({message: "This product is not found in your collection"})
+  }
+
+  return res.status(200).json({item})
+
+ }
+
+ if (user.data.role == 'buyer'){
+  const item = await findProduct(itemId);
+  return res.status(200).json({item})
+
+ }
+
+};
+
+const retrieveItems = async (req, res) => {
+  const itemId = req.params.id;
+  const token = req.header('Authorization').split(' ')[1];
+ const user = verfyToken(token, process.env.JWT_SECRET);
+console.log(user.data.role);
+ if (user.data.role == 'seller'){
+  const items = await findProducts('seller',user.data.id);
+  console.log(items);
+
+  
+  return res.status(200).json({items})
+
+ }
+
+ if (user.data.role == 'buyer'){
+  const items = await findProducts()
+  return res.status(200).json({items})
+
+ }
+
+}
+
+export { createProducts, retrieveItem, retrieveItems };
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,7 +1,52 @@
-import { Products } from '../database/models';
+import { Products, User } from '../database/models';
 
 const createProduct = async (product) => {
   await Products.create(product);
 };
 
-export { createProduct };
+const findProduct = async (id, role, sellerId) => {
+
+  if(role == 'seller'){
+    
+   const product = await Products.findOne({where: { id, sellerId}});
+   if (product == null){
+    return false
+  }
+  return product;
+
+  }
+
+  const product = await Products.findOne({where: { id}});
+  if (product == null){
+    return false
+  }
+  return product;
+};
+
+const findProducts = async (role, sellerId) => {
+
+  if(role == 'seller'){
+    
+    const products = await Products.findAll({
+      where: {sellerId},
+      include:[
+        {
+          model: User,
+          as: 'Seller'
+        }
+      ]
+    });
+    if (products == null){
+     return false
+   }
+   return products;
+ 
+   }
+
+  const products = await Products.findAll();
+  if (products == null){
+    return false
+  }
+  return products;
+}
+export { createProduct, findProduct, findProducts };
